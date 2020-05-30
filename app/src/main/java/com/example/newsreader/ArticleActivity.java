81e7d1e5e9c8d28e34ecd1806dae47e5eac9a3ab
Pulkit <Pulkitbanta55555@gmail.com>
2020-05-30 18:55:47 +0530

    Performance Improvement

--- app/src/main/java/com/example/newsreader/ArticleActivity.java
+++ app/src/main/java/com/example/newsreader/ArticleActivity.java
@@ -3,23 +3,46 @@ package com.example.newsreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
-public class ArticleActivity extends WebViewClient {
+public class ArticleActivity extends android.app.Activity {
+
+    WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
 
-        WebView webView = findViewById(R.id.webView);
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
+        webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
 
         Intent intent = getIntent();
         webView.loadUrl(intent.getStringExtra("url"));
+    }
+
+    public class WebViewClient extends android.webkit.WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
